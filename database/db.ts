6b0e55364a337315d8f75262845d9fe782b60a3e
Mario <luix30lp@gmail.com>
2022-10-14 17:52:46 -0500

    Fix in db connection

--- database/db.ts
+++ database/db.ts
@@ -33,5 +33,6 @@ export const disconnect = async () => {
 
   if (mongoConnection.isConnected === 0) return;
   await mongoose.disconnect();
+  mongoConnection.isConnected = 0;
   console.log('desconectado');
 };
